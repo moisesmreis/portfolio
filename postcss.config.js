6c01faf2d434092e2310367aadbdef1dc8540a69
Moisés Reis <118673344+moisesmreis@users.noreply.github.com>
2023-02-17 22:14:56 -0300

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,7 @@
 module.exports = {
-  plugins: [
-    "tailwindcss",
-    [
-      "@fullhuman/postcss-purgecss",
-      process.env.NODE_ENV === "production"
-        ? {
-          content: [
-            "./pages/**/*.{js,jsx,ts,tsx}",
-            "./components/**/*.{js,jsx,ts,tsx}",
-          ],
-          defaultExtractor: (content) =>
-            content.match(/[\w-/:]+(?<!:)/g) || [],
-        }
-        : false,
-    ],
-  ],
-};
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
+  },
+}
